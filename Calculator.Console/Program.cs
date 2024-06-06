43f9538f79db85ba40620f78d8f723622a9cf993
Ramanpreet Kaur <ramanpreetk197@gmail.com>
2024-06-06 18:56:51 -0400

    Add Exit feature with some test cases.

--- Calculator.Console/Program.cs
+++ Calculator.Console/Program.cs
@@ -1,34 +1,62 @@
 ﻿using Calculator;
 
-Prompts.PrintWelcomeMenu();
-Prompts.PrintOptions();
-Console.WriteLine("Enter operation number: ");
-string? OptionChoice = Console.ReadLine();
-Console.WriteLine("Enter number 1: ");
-string? Number1 = Console.ReadLine();
-Console.WriteLine("Enter number 2: ");
-string? Number2 = Console.ReadLine();
+bool exit = false;
 
-float Number1Converted = float.Parse(Number1);
-float Number2Converted = float.Parse(Number2);
-switch(OptionChoice){
-    case "1":
-        float Sum = Evaluator.Eval("+", Number1Converted, Number2Converted);
-        Console.WriteLine($"{Number1Converted} + {Number2Converted} = {Sum}");
-        break;
+while (!exit)
+{
+    Prompts.PrintWelcomeMenu();
+    Prompts.PrintOptions();
+    Console.WriteLine("Enter operation number: ");
+    string? OptionChoice = Console.ReadLine();
 
-    case "2":
-        float Subtract = Evaluator.Eval("-", Number1Converted, Number2Converted);
-        Console.WriteLine($"{Number1Converted} - {Number2Converted} = {Subtract}");
+    if (OptionChoice == "5")
+    {
+        exit = true;
+        Console.WriteLine("Exiting the application...");
         break;
-    case "3":
-        float Product = Evaluator.Eval("*", Number1Converted, Number2Converted);
-        Console.WriteLine($"{Number1Converted} * {Number2Converted} = {Product}");
-        break;
-    case "4":
-        float Divide = Evaluator.Eval("/", Number1Converted, Number2Converted);
-        Console.WriteLine($"{Number1Converted} / {Number2Converted} = {Divide}");
-        break;
-    default:
-        throw new Exception("unimplemented");
+    }
+
+    Console.WriteLine("Enter number 1: ");
+    string? Number1 = Console.ReadLine();
+    Console.WriteLine("Enter number 2: ");
+    string? Number2 = Console.ReadLine();
+
+    if (!float.TryParse(Number1, out float Number1Converted) || !float.TryParse(Number2, out float Number2Converted))
+    {
+        Console.WriteLine("Invalid number format. Please enter valid numbers.");
+        continue;
+    }
+
+    switch (OptionChoice)
+    {
+        case "1":
+            float Sum = Evaluator.Eval("+", Number1Converted, Number2Converted);
+            Console.WriteLine($"{Number1Converted} + {Number2Converted} = {Sum}");
+            break;
+        case "2":
+            float Subtract = Evaluator.Eval("-", Number1Converted, Number2Converted);
+            Console.WriteLine($"{Number1Converted} - {Number2Converted} = {Subtract}");
+            break;
+        case "3":
+            float Product = Evaluator.Eval("*", Number1Converted, Number2Converted);
+            Console.WriteLine($"{Number1Converted} * {Number2Converted} = {Product}");
+            break;
+        case "4":
+            if (Number2Converted != 0)
+            {
+                float Divide = Evaluator.Eval("/", Number1Converted, Number2Converted);
+                Console.WriteLine($"{Number1Converted} / {Number2Converted} = {Divide}");
+            }
+            else
+            {
+                Console.WriteLine("Cannot divide by zero. Please enter a valid number for division.");
+            }
+            break;
+        default:
+            Console.WriteLine("Unimplemented option. Please enter a valid option.");
+            break;
+    }
+
+    Console.WriteLine("Press Enter to continue...");
+    Console.ReadLine(); 
 }
--- Calculator.Tests/CalculatorTests.cs
+++ Calculator.Tests/CalculatorTests.cs
@@ -5,82 +5,101 @@ public class CalculatorTests
     [Fact]
     public void TestAdd()
     {
-        Assert.Equal(6L, Add.Eval(1L, 5L));
-    }
-    [Fact]
-    public void TestAdd2()
-    {
-        Assert.Equal(25L, Add.Eval(22L, 3L));
-    }
-    [Fact]
-    public void TestAdd3()
-    {
-        Assert.Equal(41L, Add.Eval(33L, 8L));
+        Assert.Equal(28L, Add.Eval(23L, 5L));
     }
     [Fact]
     public void TestSubtract()
     {
-        Assert.Equal(44L, Subtract.Eval(51L, 7L));
+        Assert.Equal(20L, Subtract.Eval(35L, 15L));
     }
-    [Fact]
-    public void TestSubtract2()
-    {
-        Assert.Equal(6L, Subtract.Eval(14L, 8L));
-    }
-    [Fact]
-    public void TestSubtract3()
-    {
-         Assert.Equal(37L, Subtract.Eval(43L, 6L));
-    }
-    
     [Fact]
     public void TestMult()
     {
-        Assert.Equal(12L, Multiply.Eval(4L, 3L));
-    }
-    [Fact]
-    public void TestMult2()
-    {
-        Assert.Equal(32L, Multiply.Eval(8L, 4L));
-    }
-    [Fact]
-    public void TestMult3()
-    {
-        Assert.Equal(36L, Multiply.Eval(9L, 4L));
+        Assert.Equal(15L, Multiply.Eval(5L, 3L));
     }
     [Fact]
     public void TestDivide()
     {
-        Assert.Equal(3L, Divide.Eval(24L, 8L));
+        Assert.Equal(6L, Divide.Eval(36L, 6L));
+    }
+    [Fact]
+    public void TestAdd2()
+    {
+        Assert.Equal(17L, Add.Eval(12L, 5L));
+    }
+    [Fact]
+    public void TestSubtract2()
+    {
+        Assert.Equal(22L, Subtract.Eval(27L, 5L));
+    }
+    [Fact]
+    public void TestMult2()
+    {
+        Assert.Equal(56L, Multiply.Eval(8L, 7L));
     }
     [Fact]
     public void TestDivide2()
     {
-        Assert.Equal(5L, Divide.Eval(25L, 5L));
+        Assert.Equal(9L, Divide.Eval(45L, 5L));
+    }
+    [Fact]
+    public void TestAdd3()
+    {
+        Assert.Equal(69L, Add.Eval(66L, 3L));
+    }
+    [Fact]
+    public void TestSubtract3()
+    {
+        Assert.Equal(2L, Subtract.Eval(8L, 6L));
+    }
+    [Fact]
+    public void TestMult3()
+    {
+        Assert.Equal(12L, Multiply.Eval(3L, 4L));
     }
     [Fact]
     public void TestDivide3()
     {
-        Assert.Equal(6L, Divide.Eval(48L, 8L));
+        Assert.Equal(7L, Divide.Eval(35L, 5L));
     }
     [Fact]
     public void TestAddOperation()
     {
-        Assert.Equal(8, Evaluator.Eval("+", 6, 2));
+        Assert.Equal(11, Evaluator.Eval("+", 9, 2));
     }
     [Fact]
     public void TestSubtractOperation()
     {
-        Assert.Equal(12, Evaluator.Eval("-", 16, 4));
+        Assert.Equal(3, Evaluator.Eval("-", 11, 8));
     }
     [Fact]
     public void TestMultiplyOperation()
     {
-        Assert.Equal(12, Evaluator.Eval("*", 6, 2));
+        Assert.Equal(81, Evaluator.Eval("*", 9, 9));
     }
     [Fact]
     public void TestDivideOperation()
     {
-        Assert.Equal(4, Evaluator.Eval("/", 32, 8));
+        Assert.Equal(9, Evaluator.Eval("/", 81, 9));
+    }
+
+    [Fact]
+    public void TestInvalidOperation()
+    {
+        Assert.Throws<Exception>(() => Evaluator.Eval("%", 9, 9));
+    }
+
+    [Fact]
+    public void TestContinueOperations()
+    {
+
+        float result1 = Evaluator.Eval("+", 5, 10);
+        Assert.Equal(15, result1);
+        float result2 = Evaluator.Eval("-", result1, 5);
+        Assert.Equal(10, result2);
+        float result3 = Evaluator.Eval("*", result2, 2);
+        Assert.Equal(20, result3);
+        float result4 = Evaluator.Eval("/", result3, 4);
+        Assert.Equal(5, result4);
     }
 }
